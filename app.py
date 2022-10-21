a0ba4a62469573c3ca3e4e1c40541a9097e9dfd1
Mirac Seref <dev.miracseref@gmail.com>
2022-10-21 12:26:08 +0300

    first working release

---
+++ app.py
@@ -0,0 +1,36 @@
+import streamlit as st
+import whisper
+import pickle
+
+st.title("WhisperAi WebApp")
+
+model_size = st.selectbox("Select model size", [
+                          'tiny', 'base', 'small', 'medium', 'large'])
+
+model = whisper.load_model(model_size)
+
+sample = st.radio("Choose audio file", [
+                  'Example 1', 'Example 2', 'Example 3', "Specific Url"])
+
+audio = False
+ex_1 = "https://datasets-server.huggingface.co/assets/google/fleurs/--/en_us/train/12/audio/audio.mp3"
+ex_2 = "https://datasets-server.huggingface.co/assets/google/fleurs/--/en_us/train/14/audio/audio.mp3"
+ex_3 = "https://datasets-server.huggingface.co/assets/google/fleurs/--/en_us/train/24/audio/audio.mp3"
+
+if sample == "Example 1":
+    audio = ex_1
+elif sample == "Example 2":
+    audio = ex_2
+elif sample == "Example 3":
+    audio = ex_3
+elif sample == "Specific Url":
+    audio = st.text_input("Paste your audio file's url")
+
+if audio:
+    # Display audio
+    st.audio(audio, format='audio/wav')
+
+    # Run the model and show the result
+    output = model.transcribe(audio)
+    if output:
+        st.json(output)
